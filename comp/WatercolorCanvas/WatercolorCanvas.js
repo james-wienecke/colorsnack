9df530ab45fac5fdec9fb91706c37f8afd546fb9
James Wienecke <james.a.wienecke@gmail.com>
2022-05-04 15:09:49 -0500

    Testing drawing on canvas.

--- comp/WatercolorCanvas/WatercolorCanvas.js
+++ comp/WatercolorCanvas/WatercolorCanvas.js
@@ -4,8 +4,13 @@ import Canvas from '../Canvas';
 
 const WatercolorCanvas = () => {
   const draw = ctx => {
+    // background
     ctx.fillStyle = '#000000';
     ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.fill();
+    // testing :)
+    ctx.fillStyle = '#FFFFFF';
+    ctx.fillRect(100, 100, ctx.canvas.width - 200, ctx.canvas.height - 200);
     ctx.fill();
   };
 
