fd09817a2b1d93f9dba3d03d386dda794c799210
James Wienecke <james.a.wienecke@gmail.com>
2022-05-06 17:44:20 -0500

    Moved more of canvas/drawing logic to canvas component.

--- comp/WatercolorCanvas/WatercolorCanvas.js
+++ comp/WatercolorCanvas/WatercolorCanvas.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import { View, useWindowDimensions } from 'react-native';
+import { useWindowDimensions } from 'react-native';
 import { StatusBarHeight } from '../../util';
 import Canvas from '../Canvas';
 
 const WatercolorCanvas = () => {
-  const draw = ctx => {
-    // background
-    ctx.fillStyle = '#000000';
-    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    ctx.fill();
-  };
-
   const windowDims = useWindowDimensions();
 
   return (
-    <View>
-      <Canvas
-        draw={draw}
-        width={windowDims.width}
-        height={windowDims.height - StatusBarHeight}
-      />
-    </View>
+    <Canvas
+      width={windowDims.width}
+      height={windowDims.height - StatusBarHeight}
+    />
   );
 };
 
