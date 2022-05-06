3e84b74190c286d1b566a9ea156f8397076aa55c
James Wienecke <james.a.wienecke@gmail.com>
2022-05-06 15:07:23 -0500

    Fixed dependencies.

--- comp/Canvas/useCanvas.js
+++ comp/Canvas/useCanvas.js
@@ -2,21 +2,20 @@ import { useRef, useEffect } from 'react';
 
 const useCanvas = (draw, options) => {
   const canvasRef = useRef(null);
-  const { width, height } = options;
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext('2d');
 
-    canvas.width = width;
-    canvas.height = height;
+    canvas.width = options.width;
+    canvas.height = options.height;
 
     const render = () => {
       draw(context);
     };
 
     render();
-  }, [draw]);
+  }, [draw, options]);
 
   return canvasRef;
 };
