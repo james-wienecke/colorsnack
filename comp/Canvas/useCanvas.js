84f32aa6522c74e57cd1af31cfc01bd65b5ef6e8
James Wienecke <james.a.wienecke@gmail.com>
2022-05-06 17:44:42 -0500

    Improved useCanvas custom hook.

--- comp/Canvas/useCanvas.js
+++ comp/Canvas/useCanvas.js
@@ -1,21 +1,36 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 
 const useCanvas = (draw, options) => {
   const canvasRef = useRef(null);
+  const contextRef = useRef(null);
+  const [canvasWidth, setCanvasWidth] = useState(options.width);
+  const [canvasHeight, setCanvasHeight] = useState(options.height);
 
+  // canvas setup
   useEffect(() => {
     const canvas = canvasRef.current;
-    const context = canvas.getContext('2d');
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
 
-    canvas.width = options.width;
-    canvas.height = options.height;
+    contextRef.current = canvas.getContext('2d');
+    const initialCanvas = ctx => {
+      // background
+      ctx.fillStyle = '#FFFFFF';
+      ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+      ctx.fill();
+      ctx.strokeStyle = '#000000';
+      ctx.lineWidth = 5;
+    };
+    initialCanvas(contextRef.current);
+  }, []);
 
+  useEffect(() => {
     const render = () => {
-      draw(context);
+      draw(contextRef.current);
     };
 
     render();
-  }, [draw, options]);
+  }, [draw]);
 
   return canvasRef;
 };
